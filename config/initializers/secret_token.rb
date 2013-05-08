07fbd883e5f86f632bfebcd5489cb0dfa60680ba
Saghir A. Khatri <saghir.alam@musewerx.com>
2013-05-08 16:51:38 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductRecall::Application.config.secret_token = '87afa027b113ccfccbc643df629e314d0014201d76bfb925a3c85ff575cb9897c38be6b94ee57b0e39204a90435aa7f25e4b009eb969198f0c1ff5b5f8659007'
